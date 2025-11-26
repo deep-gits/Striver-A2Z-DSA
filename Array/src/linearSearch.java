d3ee6b3c921e563adbc54d418a3d263a83c05b70
deep-gits <deepaksaraswat8913@gmail.com>
2025-11-26 08:09:20 +0530

    DSA

---
+++ Array/src/linearSearch.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class linearSearch {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the target value to search: ");
+        int target = sc.nextInt();
+
+        int result = search(nums, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
